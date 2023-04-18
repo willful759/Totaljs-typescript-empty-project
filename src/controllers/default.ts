6f9c639bca71cd92dff3ab8fb66a56cb93f1ba38
willful759 <ivanpineda24061@gmail.com>
2023-04-18 10:24:02 -0600

    Updated to newer totaljs version with better type support

--- src/controllers/default.ts
+++ src/controllers/default.ts
@@ -1,13 +1,11 @@
 // default Totaljs controller
 
-// totaljs uses a lot of globals, it is necesary to declare them in typescript first
-declare const ROUTE: Function;
-
 // this corresponds to the export.install = function () { ... } totaljs idiom
+
 export function install() {
   ROUTE("GET /");
-  ROUTE("GET /example1", "example1");
-  ROUTE("GET /example2", "example2");
+  ROUTE("GET /example1",'example1');
+  ROUTE("GET /example2", 'example2');
 }
 
 
